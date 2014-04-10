983e72fa2dcc514263feeebdf4b4073708247cf8
Maciej Nowakowski <m.nowakowskipl@gmail.com>
2014-04-10 16:10:01 +0100

    Finished static pages layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,44 +4,45 @@ describe "Static pages" do
 
   let(:base_title) { "Photo" }
 
+  subject { page }
+
   describe "Home page" do
 
-    it "should have the content 'Photo'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Photo')
-    end
+    before { visit root_path }
+
+    it { should have_content('Photo') } 
+    it { should have_title(full_title('')) }
+    it { should_not have_title("| Home") }
 
     it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("#{base_title}")
-end
+      expect(page).to have_title("#{base_title}")
+    end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before {  visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help')}
+    it { should have_title('Help')}
 
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path}
 
-     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it { should have_content('About Us')}
+    it { should have_title(full_title('About Us')) }
 
   end
+
+  describe "Contact page" do
+
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+
 end
